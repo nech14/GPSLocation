a8c26e00dac4137de3ddb4485b25a09030d54660
nech14 <nech14@mail.ru>
2024-04-06 21:06:44 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/gpslocation/MainActivity.kt
+++ app/src/main/java/com/example/gpslocation/MainActivity.kt
@@ -11,23 +11,59 @@ import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity(), LocationListener {
     val LOCATION_PERM_CODE = 2
+    lateinit var locationManager: LocationManager
+    lateinit var updateButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        updateButton = findViewById(R.id.updButton)
 
         // запрашиваем разрешения на доступ к геопозиции
         if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
             // переход в запрос разрешений
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERM_CODE)
+        } else{
+            LocationUpdate()
+        }
+
+        updateButton.setOnClickListener {
+            displayCoord(0.toDouble(), 0.toDouble())
+            LocationUpdate()
+            Toast.makeText(this, "Update", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    fun LocationUpdate(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
 
@@ -39,14 +75,13 @@ class MainActivity : AppCompatActivity(), LocationListener {
                 displayCoord(location.latitude, location.longitude)
             Log.d("mytag", "location set")
         }
-
     }
 
     override fun onLocationChanged(loc: Location) {
         val lat = loc.latitude
         val lng = loc.longitude
         displayCoord(lat, lng)
-        Log.d("my", "lat " + lat + " long " + lng)
+        Log.d("my", "lat: $lat long: $lng")
     }
 
     fun displayCoord(latitude: Double, longtitude: Double) {
@@ -54,8 +89,31 @@ class MainActivity : AppCompatActivity(), LocationListener {
         findViewById<TextView>(R.id.lng).text = String.format("%.5f", longtitude)
     }
 
-    // TODO: обработать случай отключения GPS (геолокации) пользователем
-    // onProviderDisabled + onProviderEnabled
+    override fun onProviderDisabled(provider: String) {
+        Toast.makeText(this, "Provider $provider disabled", Toast.LENGTH_SHORT).show()
+        super.onProviderDisabled(provider)
+    }
 
-    // TODO: обработать возврат в активность onRequestPermissionsResult
+    override fun onProviderEnabled(provider: String) {
+        Toast.makeText(this, "Provider $provider enabled", Toast.LENGTH_SHORT).show()
+        super.onProviderEnabled(provider)
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == LOCATION_PERM_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                LocationUpdate()
+            } else{
+                Toast.makeText(this, "Location permission not granted", Toast.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERM_CODE)
+            }
+        }
+    }
 }
